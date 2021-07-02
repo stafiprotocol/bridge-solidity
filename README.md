e8b3611ebbf7bb33ab5d742c63b81e0841fc4247
fengwenjun <kael@fengwenjun.com>
2021-07-02 19:22:19 +0800

    add hardhat to deploy contract to the bsc testnet

--- README.md
+++ README.md
@@ -3,3 +3,37 @@
 Fork from [ChainBridge-Solidity](https://github.com/ChainSafe/chainbridge-solidity).
 
 Bridge uses Solidity smart contracts to enable transfers to and from EVM compatible chains. These contracts consist of a core bridge contract (Bridge.sol) and a set of handler contracts (ERC20Handler.sol, and GenericHandler.sol). The bridge contract is responsible for initiating, voting on, and executing proposed transfers. The handlers are used by the bridge contract to interact with other existing contracts.
+
+Usage
+-----
+### Install requirements with yarn:
+
+```bash
+yarn
+```
+
+### Compile
+
+```bash
+yarn hardhat compile
+```
+
+### deploy to bsc
+
+Preparation:
+- create `secrets.json` file if it does not exist and set related fields. refer to secrets.json.example.
+- modify src/deploy/deploy.js to deploy the contracts.
+- use this command to deploy to the bsc-testnet, replace the argument network to deploy to other networks.
+
+```bash
+npx hardhat run --network bsctestnet src/deploy/deploy.js
+```
+
+- verif(needed). create `arguments.js` file, refer to `arguments.js.example`, put your contract constructor-args into the module.exports.
+argument can be removed if your contract constructor has no arguments at all.
+
+```bash
+npx hardhat  verify --constructor-args arguments.js --network bsctestnet DEPLOYED_CONTRACT_ADDRESS
+```
+
+
---
+++ src/deploy/deploy.js
@@ -0,0 +1,93 @@
+/* eslint no-use-before-define: "warn" */
+const fs = require("fs");
+const chalk = require("chalk");
+const { config, ethers } = require("hardhat");
+const { utils } = require("ethers");
+const R = require("ramda");
+
+const main = async () => {
+
+  console.log("\n\n 📡 Deploying...\n");
+
+  /// 注意！注意！: 部署过的合约注释掉，以避免重复部署。
+
+  // 该合约不需要参数
+  // const FISToken = await deploy("FISToken");
+  // const RFISToken = await deploy("RFISToken");
+
+  /// add in constructor args like Bridge or erc20Handler
+  // const Bridge = await deploy("Bridge", [3, ["0xBd39f5936969828eD9315220659cD11129071814", "0xBca9567A9e8D5F6F58C419d32aF6190F74C880e6"], 2, 0, 100000]) // <-- add in constructor args like line 16 vvvv
+  const ERC20Handler = await deploy("ERC20Handler", ["0xb5Dc44e4e680d9a485fCbe1f465eA0F65287FBc0"])
+
+  console.log(
+    " 💾  Artifacts (address, abi, and args) saved to: ",
+    chalk.blue("packages/hardhat/artifacts/"),
+    "\n\n"
+  );
+};
+
+const deploy = async (contractName, _args) => {
+  console.log(` 🛰  Deploying: ${contractName}`);
+
+  const contractArgs = _args || [];
+  const contractArtifacts = await ethers.getContractFactory(contractName);
+  const deployed = await contractArtifacts.deploy(...contractArgs);
+  const encoded = abiEncodeArgs(deployed, contractArgs);
+  fs.writeFileSync(`artifacts/${contractName}.address`, deployed.address);
+
+  console.log(
+    " 📄",
+    chalk.cyan(contractName),
+    "deployed to:",
+    chalk.magenta(deployed.address),
+  );
+
+  if (!encoded || encoded.length <= 2) return deployed;
+  fs.writeFileSync(`artifacts/${contractName}.args`, encoded.slice(2));
+
+  return deployed;
+};
+
+// ------ utils -------
+
+// abi encodes contract arguments
+// useful when you want to manually verify the contracts
+// for example, on Etherscan
+const abiEncodeArgs = (deployed, contractArgs) => {
+  // not writing abi encoded args if this does not pass
+  if (
+    !contractArgs ||
+    !deployed ||
+    !R.hasPath(["interface", "deploy"], deployed)
+  ) {
+    return "";
+  }
+  const encoded = utils.defaultAbiCoder.encode(
+    deployed.interface.deploy.inputs,
+    contractArgs
+  );
+  return encoded;
+};
+
+// checks if it is a Solidity file
+const isSolidity = (fileName) =>
+  fileName.indexOf(".sol") >= 0 && fileName.indexOf(".swp") < 0;
+
+const readArgsFile = (contractName) => {
+  let args = [];
+  try {
+    const argsFile = `./contracts/${contractName}.args`;
+    if (!fs.existsSync(argsFile)) return args;
+    args = JSON.parse(fs.readFileSync(argsFile));
+  } catch (e) {
+    console.log(e);
+  }
+  return args;
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
